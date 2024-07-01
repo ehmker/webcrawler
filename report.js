dcd359588f61f1dbb057a0c5eaaa3a3a80c13eb0
ehmker <ehmker@proton.me>
2024-07-01 01:36:07 -0500

    added printReport

--- report.js
+++ report.js
@@ -1,11 +1,16 @@
 function printReport(pages){
+    console.log('===================')
     console.log('Crawled Page Report')
+    console.log('===================')
+
     const sorted_pages = sortObject(pages)
     console.log('sorted pages=',sorted_pages)
     for (let i = 0; i < sorted_pages.length; i++)
         console.log(`Found ${sorted_pages[i]['value']} internal links to ${sorted_pages[i]['key']}`) 
 
 }
+
+//intakes an object and converts to an array of objects and returns a sorted array
 function sortObject(obj){
     let arr = []
     for (let prop in obj){
